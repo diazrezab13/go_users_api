7f28068f03376eb92ee8a54f39cc2e33f58625ee
diazrezab13 <diazrezab@gmail.com>
2022-01-01 13:03:31 +0700

    implement create user service (always return request) and json error handler with bad request struct

--- controllers/users_controller.go
+++ controllers/users_controller.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"go_users_api/domain/users"
+	"go_users_api/services"
+	"go_users_api/utils/errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,7 +14,21 @@ var (
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me")
+	var user users.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,10 @@
 package services
+
+import (
+	"go_users_api/domain/users"
+	"go_users_api/utils/errors"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return &user, nil
+}
